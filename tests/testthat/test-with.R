130adbd424f293211283ea4514ddbca49b487a1e
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-14 15:18:06 +0200

    explicit tests for with_path

--- tests/testthat/test-with.R
+++ tests/testthat/test-with.R
@@ -92,6 +92,34 @@ test_that("with_path works and resets path", {
   expect_equal(current, get_path())
 })
 
+test_that("with_path with suffix action works and resets path", {
+  current <- normalizePath(get_path())
+  new_path <- normalizePath(".")
+  with_path(
+    new_path,
+    action = "suffix",
+    {
+      expect_equal(normalizePath(new_path), tail(get_path(), n = 1))
+      expect_equal(length(get_path()), length(current) + 1L)
+    }
+  )
+  expect_equal(current, get_path())
+})
+
+test_that("with_path with replace action works and resets path", {
+  current <- normalizePath(get_path())
+  new_path <- normalizePath(".")
+  with_path(
+    new_path,
+    action = "replace",
+    {
+      expect_equal(normalizePath(new_path), get_path())
+      expect_equal(length(get_path()), 1L)
+    }
+  )
+  expect_equal(current, get_path())
+})
+
 test_that("with_libpaths works and resets library", {
   lib <- .libPaths()
   new_lib <- "."
