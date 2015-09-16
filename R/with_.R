37a25ce9844339ebff11dde230244eeda4278801
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-16 23:01:52 +0200

    with_ is internal, refine docs

--- R/with_.R
+++ R/with_.R
@@ -1,4 +1,9 @@
-#' Execute code in temporarily altered environment.
+#' Create a new "with" function
+#'
+#' This function is a "constructor" for \code{with_...} functions.  This
+#' function is only needed if you want to alter some global state which is
+#' not covered by the existing \code{with_...} functions, see
+#' \link{withr-package} for an overview.
 #'
 #' @param set Function used to set the state.
 #' @param reset Function used to reset the state.
@@ -9,7 +14,9 @@
 #' \item \code{code}: Code to run in that state.
 #' }
 #' If there are more arguments to the function passed in \code{set} they are
-#' added to the returned function.
+#' added to the returned function.  If \code{set} does not have arguments,
+#' the returned function only has a \code{code} argument.
+#' @keywords internal
 #' @export
 with_ <- function(set, reset = set, envir = parent.frame()) {
 
