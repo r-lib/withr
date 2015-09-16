9abedc1e1727250afe0f14cf4fe3245441e57d42
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-16 22:23:29 +0200

    with_something works with functions that have no arguments

--- R/with_something.R
+++ R/with_something.R
@@ -15,11 +15,20 @@ with_something <- function(set, reset = set, envir = parent.frame()) {
 
   fmls <- formals(set)
 
-  # called pass all extra formals on
-  called_fmls <- setNames(lapply(names(fmls), as.symbol), names(fmls))
+  if (length(fmls) > 0L) {
+    # called pass all extra formals on
+    called_fmls <- setNames(lapply(names(fmls), as.symbol), names(fmls))
 
-  # rename first formal to new
-  called_fmls[[1]] <- as.symbol("new")
+    # rename first formal to new
+    called_fmls[[1]] <- as.symbol("new")
+
+    fun_args <- c(alist(new =, code =), fmls[-1L])
+  } else {
+    # no formals -- only have code
+    called_fmls <- NULL
+
+    fun_args <- alist(code =)
+  }
 
   set_call <- as.call(c(substitute(set), called_fmls))
 
@@ -31,7 +40,7 @@ with_something <- function(set, reset = set, envir = parent.frame()) {
           reset = if (missing(reset)) substitute(set) else substitute(reset))))
 
   # substitute does not work on arguments, so we need to fix them manually
-  formals(fun) <- c(alist(new =, code =), fmls[-1])
+  formals(fun) <- fun_args
 
   environment(fun) <- envir
 
