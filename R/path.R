cd59eafc4b01ff49299193613b5e7fe6c0277d80
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-16 09:09:36 +0200

    unexport set_path

--- R/path.R
+++ R/path.R
@@ -1,26 +1,9 @@
-#' @include with_something.R
-
-#' @title Get/set the PATH variable.
-#' @param path character vector of paths
-#' @return \code{set_path} invisibly returns the old path.
-#' @name path
-#' @family path
-#' @seealso \code{\link{with_path}} to temporarily set the path for a block
-#'   of code
-NULL
-
 # path -----------------------------------------------------------------------
 
-# @rdname path
 get_path <- function() {
   strsplit(Sys.getenv("PATH"), .Platform$path.sep)[[1]]
 }
 
-#' @export
-#' @param action One of \code{"prefix"} (default), \code{"suffix"} or \code{"replace"},
-#'   determines if the existing paths should be prepended, appended, or replaced by the
-#'   existing paths.
-#' @rdname path
 set_path <- function(path, action = "prefix") {
   path <- normalizePath(path, mustWork = FALSE)
 
