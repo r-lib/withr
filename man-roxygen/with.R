3c2c439a8f3e9eb2bc4cf17dbc56c27c18173a6d
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-21 22:59:03 +0200

    document return type for all with_ functions

--- man-roxygen/with.R
+++ man-roxygen/with.R
@@ -1,2 +1,4 @@
 #' @param code \code{[any]}\cr Code to execute in the temporary environment
+#' @return \code{[any]}\cr The results of the evaluation of the \code{code}
+#'   argument.
 #' @seealso \code{\link{withr}} for examples
