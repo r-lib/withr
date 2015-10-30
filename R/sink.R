8edfe435f3353d9b684c2260e83aaefbc70af166
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-30 18:21:12 +0100

    use type convention

--- R/sink.R
+++ R/sink.R
@@ -5,8 +5,9 @@
 #' Temporarily divert output to a file via \code{\link{sink}}.
 #'
 #' @template with
-#' @param new a writable \link{connection} or a character string naming the file
-#'   to write to, or \code{NULL} to stop sink-ing.
+#' @param new \code{[character(1)|connection]}\cr
+#'   A writable \link{connection} or a character string naming the file to write
+#'   to, or \code{NULL} to stop sink-ing.
 #' @inheritParams base::sink
 #' @seealso \code{\link{sink}}
 #' @export
