bef55f43dcaa6962e2147eb96459aebe74683e5c
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-30 18:18:33 +0100

    document new parameter

--- R/sink.R
+++ R/sink.R
@@ -5,6 +5,8 @@
 #' Temporarily divert output to a file via \code{\link{sink}}.
 #'
 #' @template with
+#' @param new a writable \link{connection} or a character string naming the file
+#'   to write to, or \code{NULL} to stop sink-ing.
 #' @inheritParams base::sink
 #' @seealso \code{\link{sink}}
 #' @export
