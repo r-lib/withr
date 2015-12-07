4bf094ec0fc7863ff5f170a05da29e6c66f5aa12
Jim Hester <james.f.hester@gmail.com>
2015-12-07 09:44:17 -0500

    Small tweak to get working properly on both unix and windows

--- R/windows/png.R
+++ R/windows/png.R
@@ -1,2 +1,3 @@
 png_dev <- wrap(grDevices::png, antialias <- match.arg(antialias), grDevices::dev.cur())
 jpeg_dev <- wrap(grDevices::jpeg, antialias <- match.arg(antialias), grDevices::dev.cur())
+bmp_dev <- wrap(grDevices::bmp, antialias <- match.arg(antialias), grDevices::dev.cur())
