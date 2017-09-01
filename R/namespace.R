e8f245854e6b88a7a92383dd7803374c0baa7b96
Jim Hester <james.f.hester@gmail.com>
2017-09-01 10:12:59 -0400

    Rename with_namespace to with_package, add real with_namespace
    This more accurately reflects what the functions do

--- R/namespace.R
+++ R/namespace.R
@@ -1,18 +1,46 @@
 #' Execute code with a modifed search path
 #'
-#' This attaches a namespace to the search path, executes the code, then
+#' `with_package()` attaches a package to the search path, executes the code, then
 #' removes the package from the search path. The package namespace is _not_
-#' unloaded however.
-#' @param namespace \code{[character(1)]}\cr Namespace / package name to load.
+#' unloaded however. `with_namespace()` does the same thing, but attaches the
+#' package namespace to the search path, so all objects (even unexported ones) are also
+#' available on the search path.
+#' @param package \code{[character(1)]}\cr package name to load.
+#' @inheritParams defer
 #' @template with
 #' @examples
 #' \dontrun{
-#' with_namespace("ggplot2", {
+#' with_package("ggplot2", {
 #'   ggplot(mtcars) + geom_point(aes(wt, hp))
 #' })
 #' }
-with_namespace <- function(namespace, code) {
-  library(namespace, character.only = TRUE)
-  on.exit(detach(paste0("package:", namespace), character.only = TRUE))
+#' @export
+with_package <- function(package, code) {
+  library(package, character.only = TRUE)
+  on.exit(detach(paste0("package:", package), character.only = TRUE))
   force(code)
+}
+
+#' @rdname with_package
+#' @export
+local_package <- function(package, envir = parent.frame()) {
+  library(package, character.only = TRUE)
+  defer(detach(paste0("package:", package), character.only = TRUE), envir = envir)
+}
+
+#' @rdname with_package
+#' @export
+with_namespace <- function(package, code) {
+  name <- paste0("asNamespace(\"", package, "\")")
+  (get("attach"))(asNamespace(package), name = name)
+  on.exit(detach(name, character.only = TRUE))
+  force(code)
+}
+
+#' @rdname with_package
+#' @export
+local_namespace <- function(package, envir = parent.frame()) {
+  name <- paste0("asNamespace(\"", package, "\")")
+  (get("attach"))(asNamespace(package), name = name)
+  defer(detach(name, character.only = TRUE), envir = envir)
 }
--- tests/testthat/test-namespace.R
+++ tests/testthat/test-namespace.R
@@ -1,13 +1,51 @@
 context("namespace")
 
-test_that("with_namespace works", {
+test_that("with_package works", {
 
-  # tools namespace not attached to the search path
+  # tools package not attached to the search path
   expect_false("package:tools" %in% search())
 
-  with_namespace("tools",
+  with_package("tools",
+
+    # SIGINT is an exported object in tools
     expect_equal(SIGINT, 2))
 
-  # tools namespace still not attached to the search path
+  # tools package still not attached to the search path
   expect_false("package:tools" %in% search())
+})
+
+test_that("local_package works", {
+
+  # tools package not attached to the search path
+  expect_false("package:tools" %in% search())
+
+  f <- function() {
+    local_package("tools")
+
+    # SIGINT is an exported object in tools
+    expect_equal(SIGINT, 2)
+  }
+
+  f()
+
+  # tools package still not attached to the search path
+  expect_false("package:tools" %in% search())
+})
+
+test_that("local_namespace works", {
+
+  # tools package not attached to the search path
+  expect_false("asNamespace(\"tools\")" %in% search())
+
+  f <- function() {
+    local_namespace("tools")
+
+    # .BioC_version_associated_with_R_version is a non-exported object in tools
+    expect_true(is.function(.BioC_version_associated_with_R_version))
+  }
+
+  f()
+
+  # tools namespace still not attached to the search path
+  expect_false("asNamespace(\"tools\")" %in% search())
 })
