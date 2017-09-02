eff4818b03859729e756142494fa164620522877
Jim Hester <james.f.hester@gmail.com>
2017-09-02 05:54:27 -0400

    Commit missing files

---
+++ R/torture.R
@@ -0,0 +1,13 @@
+#' Torture Garbage Collector
+#'
+#' Temporarily turn gctorture2 on.
+#'
+#' @template with
+#' @param new `[integer]`\cr run GC every 'step' allocations.
+#' @inheritParams base::gctorture
+#' @inheritParams local_
+with_gctorture2 <- with_(gctorture2)
+formals(with_gctorture2)[[3]] <- quote(new)
+
+local_gctorture2 <- local_(gctorture2)
+formals(local_gctorture2)[[2]] <- quote(new)
