d1f095759adf1be2b52dd74e59caad39c71be063
Jim Hester <james.f.hester@gmail.com>
2017-09-15 09:50:05 -0400

    Forward all arguments for library and attach
    Forwards all optional arguments from library and attach to with_package
    and with_environment.

--- R/namespace.R
+++ R/namespace.R
@@ -7,7 +7,9 @@
 #' available on the search path.
 #' @param package \code{[character(1)]}\cr package name to load.
 #' @param env \code{[environment()]}\cr Environment to attach.
+#' @param .local_envir `[environment]`\cr The environment to use for scoping.
 #' @inheritParams defer
+#' @inheritParams base::library
 #' @template with
 #' @examples
 #' \dontrun{
@@ -16,17 +18,32 @@
 #' })
 #' }
 #' @export
-with_package <- function(package, code) {
-  suppressPackageStartupMessages(library(package, character.only = TRUE, quietly = TRUE))
+with_package <- function(package, code, help, pos = 2, lib.loc = NULL,
+  character.only = TRUE, logical.return = FALSE, warn.conflicts = TRUE,
+  quietly = TRUE, verbose = getOption("verbose")) {
+
+  suppressPackageStartupMessages(
+    (get("library"))(package, help = help, pos = pos, lib.loc = lib.loc,
+      character.only = character.only, logical.return = logical.return,
+      warn.conflicts = warn.conflicts, quietly = quietly, verbose = verbose))
+
   on.exit(detach(paste0("package:", package), character.only = TRUE))
   force(code)
 }
 
 #' @rdname with_package
 #' @export
-local_package <- function(package, envir = parent.frame()) {
-  suppressPackageStartupMessages(library(package, character.only = TRUE, quietly = TRUE))
-  defer(detach(paste0("package:", package), character.only = TRUE), envir = envir)
+local_package <- function(package, help, pos = 2, lib.loc = NULL,
+  character.only = TRUE, logical.return = FALSE, warn.conflicts = TRUE,
+  quietly = TRUE, verbose = getOption("verbose"),
+  .local_envir = parent.frame()) {
+
+  suppressPackageStartupMessages(
+    (get("library"))(package, help = help, pos = pos, lib.loc = lib.loc,
+      character.only = character.only, logical.return = logical.return,
+      warn.conflicts = warn.conflicts, quietly = quietly, verbose = verbose))
+
+  defer(detach(paste0("package:", package), character.only = TRUE), envir = .local_envir)
 }
 
 #' @rdname with_package
@@ -41,17 +58,18 @@ with_namespace <- function(package, code) {
 
 #' @rdname with_package
 #' @export
-local_namespace <- function(package, envir = parent.frame()) {
+local_namespace <- function(package, .local_envir = parent.frame()) {
   ns <- asNamespace(package)
   name <- format(ns)
   (get("attach"))(ns, name = name)
-  defer(detach(name, character.only = TRUE), envir = envir)
+  defer(detach(name, character.only = TRUE), envir = .local_envir)
 }
 
 #' @rdname with_package
+#' @inheritParams base::attach
 #' @export
-with_environment <- function(env, code) {
-  name <- format(env)
+with_environment <- function(env, code, pos = 2L, name = format(env),
+  warn.conflicts = FALSE) {
   (get("attach"))(env, name = name)
   on.exit(detach(name, character.only = TRUE))
   force(code)
@@ -59,8 +77,8 @@ with_environment <- function(env, code) {
 
 #' @rdname with_package
 #' @export
-local_environment <- function(env, envir = parent.frame()) {
-  name <- format(env)
+local_environment <- function(env, pos = 2L, name = format(env),
+  warn.conflicts = FALSE, .local_envir = parent.frame()) {
   (get("attach"))(env, name = name)
-  defer(detach(name, character.only = TRUE), envir = envir)
+  defer(detach(name, character.only = TRUE), envir = .local_envir)
 }
