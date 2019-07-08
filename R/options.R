df85c8706c89b7948b0063ba0c2f9011a0e72ae3
Ciprian Alexandru <alexcipro@yahoo.com>
2019-07-08 14:41:19 +0200

    Add examples for `with_options()`
    Add examples for `with_options()` function.

--- R/options.R
+++ R/options.R
@@ -18,6 +18,19 @@ reset_options <- function(old_options) {
 #' @param new `[named list]`\cr New options and their values
 #' @inheritParams with_collate
 #' @seealso [options()]
+#' @examples
+#' # number of significant digits to print
+#' getOption("digits")
+#' # modify temporarily the number of significant digits to print
+#' with_options(list(digits = 3), getOption("digits"))
+#' with_options(list(digits = 3), print(pi))
+#'
+#' # modify temporarily the character to be used as the decimal point
+#' getOption("digits")
+#' with_options(list(OutDec = ","), print(pi))
+#'
+#' # modify temporarily multiple options
+#' with_options(list(OutDec = ",", digits = 3), print(pi))
 #' @export
 with_options <- with_(set_options, reset_options)
 
