7818d665cab96498418492519c4f5f62413175db
Alessandro Gasparini <ellessenne@gmail.com>
2019-07-08 16:14:33 +0200

    Add examples for with_collate()

--- R/collate.R
+++ R/collate.R
@@ -12,6 +12,15 @@ set_collate <- function(locale) set_locale(c(LC_COLLATE = locale))[[1]]
 #' @template with
 #' @param new `[character(1)]`\cr New collation order
 #' @param .local_envir `[environment]`\cr The environment to use for scoping.
+#' @examples
+#' \dontrun{
+#'
+#' # Modify collation order:
+#' x <- c("bernard", "bérénice", "béatrice", "boris")
+#' withr::with_collate("fr_FR", sort(x))
+#' withr::with_collate("C", sort(x))
+#'
+#' }
 #' @export
 with_collate <- with_(set_collate)
 
