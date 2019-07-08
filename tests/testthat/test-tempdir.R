29d01c7e1751c7b7cdb1b10685f2ad5fe343d621
Zhian N. Kamvar <zkamvar@gmail.com>
2019-07-08 10:32:18 +0100

    clean up tests

--- tests/testthat/test-tempdir.R
+++ tests/testthat/test-tempdir.R
@@ -2,29 +2,20 @@ context("tempdir")
 
 test_that("clean needs to be a single logical", {
   
-  contents <- dir(tempdir())
-
-  expect_error(with_tempdir(clean = "sparkling"), 
-               "'clean' must be a single TRUE or FALSE")
-  # Check to make sure that the tempdir wasn't touched
-  expect_identical(contents, dir(tempdir()))
-  
-  expect_error(with_tempdir(clean = c(TRUE, FALSE)), 
-               "'clean' must be a single TRUE or FALSE")
-  # Check to make sure that the tempdir wasn't touched
-  expect_identical(contents, dir(tempdir()))
+  expect_error(with_tempdir(clean = "sparkling"), "TRUE or FALSE")
+  expect_error(with_tempdir(clean = c(TRUE, FALSE)), "TRUE or FALSE")
 
 })
 
 test_that("tempdir cleans up after itself", {
-  contents <- dir(tempdir())
+
   tmp <- with_tempdir(getwd())
   expect_false(file.exists(tmp))
-  # Check to make sure that the tempdir wasn't touched
-  expect_identical(contents, dir(tempdir()))
+
 })
 
 test_that("tempdir will leave the directory alone if clean = FALSE", {
+
   tmp <- with_tempdir({
     writeLines("Is it me you were looking for?", con = "hello.txt")
     getwd()
