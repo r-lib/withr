2efaa5f4c2f301442181b2b489f9002784a4069b
Dragoș Moldovan-Grünfeld <dgrunfeld@mango-solutions.com>
2019-07-08 11:37:26 +0200

    removes the example and comments as per Hadley's suggestion. I guess this PR is no longer needed since the only reason for its existence was to add with_dir() examples

--- R/dir.R
+++ R/dir.R
@@ -12,14 +12,6 @@ NULL
 #' @inheritParams with_collate
 #' @seealso [setwd()]
 #' @export
-#' @examples
-#' getwd() # current working directory
-#' 
-#' # change the current working directory to a temporary one and run getwd()
-#' with_dir(tempdir(), getwd())
-#' 
-#' # once the function execution is completed the working directory is restored
-#' getwd()
 with_dir <- with_(setwd)
 
 #' @rdname with_dir
