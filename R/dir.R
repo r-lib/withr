bd75c1cdbabee4f65b9a388e1207de6e5e8043ce
Jim Hester <james.f.hester@gmail.com>
2020-09-10 11:55:31 -0400

    Document with_tempdir with with_tempfile rather than with_dir
    We no longer change the directory, so this seems more appropriate

--- R/dir.R
+++ R/dir.R
@@ -9,8 +9,6 @@ NULL
 #'
 #' @template with
 #' @param new `[character(1)]`\cr New working directory
-#' @param clean `[logical(1)]`\cr A logical indicating if the temporary
-#'   directory should be deleted after use (`TRUE`, default) or left alone (`FALSE`).
 #' @inheritParams with_collate
 #' @seealso [setwd()]
 #' @export
@@ -23,37 +21,3 @@ with_dir <- with_(setwd)
 #' @rdname with_dir
 #' @export
 local_dir <- local_(setwd)
-
-#' @rdname with_dir
-#' @export
-with_tempdir <- function(code, clean = TRUE) {
-  if (length(clean) > 1 || !is.logical(clean)) {
-    stop("`clean` must be a single TRUE or FALSE", call. = FALSE)
-  }
-  tmp <- tempfile()
-  dir.create(tmp)
-  if (clean) {
-    on.exit(unlink(tmp, recursive = TRUE), add = TRUE)
-  }
-  withr::with_dir(tmp, code)
-}
-
-#' @rdname with_dir
-#' @inheritParams with_tempfile
-#' @export
-local_tempdir <- function(pattern = "file", tmpdir = tempdir(),
-                          fileext = "", .local_envir = parent.frame(), clean = TRUE) {
-  if (length(clean) > 1 || !is.logical(clean)) {
-    stop("`clean` must be a single TRUE or FALSE", call. = FALSE)
-  }
-
-  path <- tempfile(pattern = pattern, tmpdir = tmpdir, fileext = fileext)
-
-  dir.create(path, recursive = TRUE)
-
-  if (isTRUE(clean)) {
-    defer(unlink(path, recursive = TRUE), envir = .local_envir)
-  }
-
-  path
-}
--- R/tempfile.R
+++ R/tempfile.R
@@ -4,6 +4,9 @@
 #' @template with
 #' @param new `[character vector]`\cr (Deprecated for `local_tempfile()`) Names of temporary file handles to create.
 #' @param envir `[environment]`\cr Environment in which to define the temporary files.
+#' @param clean `[logical(1)]`\cr A logical indicating if the temporary
+#'   directory should be deleted after use (`TRUE`, default) or left alone (`FALSE`).
+#' @inheritParams with_collate
 #' @inheritParams base::tempfile
 #' @examples
 #' # check how big iris would be if written as csv vs RDS
@@ -40,4 +43,41 @@ local_tempfile <- function(new = NULL, envir = parent.frame(),
       envir = envir)
   }
   defer(unlink(mget(new, envir = envir), recursive = TRUE), envir = envir)
+}
+
+
+#' @rdname with_tempfile
+#' @export
+with_tempdir <- function(code, clean = TRUE,
+  pattern = "file", tmpdir = tempdir(), fileext = "") {
+  if (length(clean) > 1 || !is.logical(clean)) {
+    stop("`clean` must be a single TRUE or FALSE", call. = FALSE)
+  }
+
+  tmp <- tempfile(pattern = pattern, tmpdir = tmpdir, fileext = fileext)
+
+  dir.create(tmp)
+  if (clean) {
+    on.exit(unlink(tmp, recursive = TRUE), add = TRUE)
+  }
+  withr::with_dir(tmp, code)
+}
+
+#' @rdname with_tempfile
+#' @export
+local_tempdir <- function(pattern = "file", tmpdir = tempdir(),
+                          fileext = "", .local_envir = parent.frame(), clean = TRUE) {
+  if (length(clean) > 1 || !is.logical(clean)) {
+    stop("`clean` must be a single TRUE or FALSE", call. = FALSE)
+  }
+
+  path <- tempfile(pattern = pattern, tmpdir = tmpdir, fileext = fileext)
+
+  dir.create(path, recursive = TRUE)
+
+  if (isTRUE(clean)) {
+    defer(unlink(path, recursive = TRUE), envir = .local_envir)
+  }
+
+  path
 }
