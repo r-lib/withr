79cd8434ef99f3b775270d781e1d4e25cfb83666
Lionel Henry <lionel.hry@gmail.com>
2022-02-24 10:44:21 +0100

    Move `with_path()` and `local_path()` tests to `test-path.R`

--- tests/testthat/test-local.R
+++ tests/testthat/test-local.R
@@ -77,39 +77,6 @@ test_that("local_ works on functions without arguments", {
   expect_equal(res, 1L:3L)
 })
 
-test_that("local_path works and resets path", {
-  current <- normalizePath(get_path(), mustWork = FALSE)
-  new_path <- normalizePath(".")
-  local({
-    local_path(new_path)
-    expect_equal(normalizePath(new_path), head(get_path(), n = 1))
-    expect_equal(length(get_path()), length(current) + 1L)
-  })
-  expect_equal(current, get_path())
-})
-
-test_that("local_path with suffix action works and resets path", {
-  current <- normalizePath(get_path(), mustWork = FALSE)
-  new_path <- normalizePath(".")
-  local({
-    local_path(new_path, action = "suffix")
-    expect_equal(normalizePath(new_path), tail(get_path(), n = 1))
-    expect_equal(length(get_path()), length(current) + 1L)
-  })
-  expect_equal(current, get_path())
-})
-
-test_that("local_path with replace action works and resets path", {
-  current <- normalizePath(get_path(), mustWork = FALSE)
-  new_path <- normalizePath(".")
-  local({
-    local_path(new_path, action = "replace")
-    expect_equal(normalizePath(new_path), get_path())
-    expect_equal(length(get_path()), 1L)
-  })
-  expect_equal(current, get_path())
-})
-
 test_that("local_libpaths works and resets library", {
   lib <- .libPaths()
   new_lib <- "."
--- tests/testthat/test-path.R
+++ tests/testthat/test-path.R
@@ -1,3 +1,77 @@
 test_that("`action` is checked", {
   expect_snapshot_error(with_path("foo", NULL, action = "Suffix"))
 })
+
+test_that("with_path works and resets path", {
+  current <- normalizePath(get_path(), mustWork = FALSE)
+  new_path <- normalizePath(".")
+  with_path(
+    new_path,
+    {
+      expect_equal(normalizePath(new_path), head(get_path(), n = 1))
+      expect_equal(length(get_path()), length(current) + 1L)
+    }
+  )
+  expect_equal(current, get_path())
+})
+
+test_that("with_path with suffix action works and resets path", {
+  current <- normalizePath(get_path(), mustWork = FALSE)
+  new_path <- normalizePath(".")
+  with_path(
+    new_path,
+    action = "suffix",
+    {
+      expect_equal(normalizePath(new_path), tail(get_path(), n = 1))
+      expect_equal(length(get_path()), length(current) + 1L)
+    }
+  )
+  expect_equal(current, get_path())
+})
+
+test_that("with_path with replace action works and resets path", {
+  current <- normalizePath(get_path(), mustWork = FALSE)
+  new_path <- normalizePath(".")
+  with_path(
+    new_path,
+    action = "replace",
+    {
+      expect_equal(normalizePath(new_path), get_path())
+      expect_equal(length(get_path()), 1L)
+    }
+  )
+  expect_equal(current, get_path())
+})
+
+test_that("local_path works and resets path", {
+  current <- normalizePath(get_path(), mustWork = FALSE)
+  new_path <- normalizePath(".")
+  local({
+    local_path(new_path)
+    expect_equal(normalizePath(new_path), head(get_path(), n = 1))
+    expect_equal(length(get_path()), length(current) + 1L)
+  })
+  expect_equal(current, get_path())
+})
+
+test_that("local_path with suffix action works and resets path", {
+  current <- normalizePath(get_path(), mustWork = FALSE)
+  new_path <- normalizePath(".")
+  local({
+    local_path(new_path, action = "suffix")
+    expect_equal(normalizePath(new_path), tail(get_path(), n = 1))
+    expect_equal(length(get_path()), length(current) + 1L)
+  })
+  expect_equal(current, get_path())
+})
+
+test_that("local_path with replace action works and resets path", {
+  current <- normalizePath(get_path(), mustWork = FALSE)
+  new_path <- normalizePath(".")
+  local({
+    local_path(new_path, action = "replace")
+    expect_equal(normalizePath(new_path), get_path())
+    expect_equal(length(get_path()), 1L)
+  })
+  expect_equal(current, get_path())
+})
