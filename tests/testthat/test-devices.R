3f5f82ae9a6cb5d2ab3fa80c66d868289113be14
Hadley Wickham <h.wickham@gmail.com>
2024-07-29 15:58:45 -0500

    Simplify devices tests

--- tests/testthat/test-devices.R
+++ tests/testthat/test-devices.R
@@ -6,61 +6,36 @@ skip_if_needs_cairo <- function(fn) {
 }
 
 test_that("with_*device* functions create a plot file", {
-  skip_if_not_installed("lattice")
-  # A plot
-  p <- lattice::xyplot(y ~ x, data.frame(x = -2:2, y = dnorm(-2:2)))
-
-  # A directory to store the plots
-  plot_dir <- tempfile("withr-test-plots-")
-  dir.create(plot_dir)
-
   fn_names <- c("with_bmp", "with_jpeg", "with_pdf", "with_png",
     "with_tiff", "with_svg", "with_cairo_pdf", "with_cairo_ps")
 
   fns <- mget(fn_names, envir = asNamespace("withr"))
-  extensions <- c("bmp", "pdf", "ps", "jpg", "pdf", "png", "tiff", "svg", "pdf", "ps")
   for (i in seq_along(fns)) {
     skip_if_needs_cairo(fn_names[[i]])
 
-    filename <- file.path(plot_dir, paste0("test-", fn_names[i], ".", extensions[i]))
-    info <- paste0("function = ", fn_names[i], "; filename = ", filename)
-    expect_silent(fns[[i]](filename, print(p)))
-    expect_true(file.exists(filename), info = info)
-    expect_gt(file.info(filename)$size, 0, label = info)
+    filename <- withr::local_tempfile()
+    expect_no_error(fns[[i]](filename, plot(1)))
+    expect_true(file.exists(filename), info = fn_names[[i]])
+    expect_gt(file.info(filename)$size, 0, label = fn_names[[i]])
   }
-
-  unlink(plot_dir)
 })
 
 test_that("local_device functions create a plot file", {
-  skip_if_not_installed("lattice")
-  # A plot
-  p <- lattice::xyplot(y ~ x, data.frame(x = -2:2, y = dnorm(-2:2)))
-
-  # A directory to store the plots
-  plot_dir <- tempfile("withr-test-plots-local-")
-  dir.create(plot_dir)
-
   fn_names <- c("local_bmp", "local_jpeg", "local_pdf", "local_png",
     "local_tiff", "local_svg", "local_cairo_pdf", "local_cairo_ps")
 
   fns <- mget(fn_names, envir = asNamespace("withr"))
-  extensions <- c("bmp", "pdf", "ps", "jpg", "pdf", "png", "tiff", "svg", "pdf", "ps")
-
   for (i in seq_along(fns)) {
     skip_if_needs_cairo(fn_names[[i]])
 
-    filename <- file.path(plot_dir, paste0("test-", fn_names[i], ".", extensions[i]))
-    info <- paste0("function = ", fn_names[i], "; filename = ", filename)
-    (function(i) {
-      expect_silent(fns[[i]](filename))
-      print(p)
-    })(i)
-    expect_true(file.exists(filename), info = info)
-    expect_gt(file.info(filename)$size, 0, label = info)
+    filename <- withr::local_tempfile()
+    local({
+      expect_no_error(fns[[i]](filename))
+      plot(1)
+    })
+    expect_true(file.exists(filename), info = fn_names[[i]])
+    expect_gt(file.info(filename)$size, 0, label = fn_names[[i]])
   }
-
-  unlink(plot_dir)
 })
 
 test_that("multiple devices closed in correct order", {
