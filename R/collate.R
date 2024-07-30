8f469032bfdaed7c1a0e1d6df7bb3ff50ab679d8
Lionel Henry <lionel.hry@gmail.com>
2024-07-30 12:20:45 +0200

    Hardcode collation example
    Closes #266

--- R/collate.R
+++ R/collate.R
@@ -6,13 +6,19 @@
 #' @template with
 #' @param new `[character(1)]`\cr New collation order
 #' @param .local_envir `[environment]`\cr The environment to use for scoping.
-#' @examples
+#' 
+#' @section Examples:
 #'
+#' ```
 #' # Modify collation order:
 #' x <- c("bernard", "bérénice", "béatrice", "boris")
-#' with_collate("fr_FR", sort(x))
-#' with_collate("C", sort(x))
 #'
+#' with_collate("fr_FR", sort(x))
+#' #> [1] "béatrice" "bérénice" "bernard"  "boris"   
+#'
+#' with_collate("C", sort(x))
+#' #> [1] "bernard"  "boris"    "béatrice" "bérénice"
+#' ```
 #' @export
 with_collate <- function(new, code) {
   with_locale(c(LC_COLLATE = new), code)
